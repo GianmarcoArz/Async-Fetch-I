9c6f6b4ec339d89c596eaa330721e2d9c12f3827
GianmarcoArz <gianmarcoarzanese@gmail.com>
2024-09-25 16:04:21 +0200

    complete with extra 1

--- librey.js
+++ librey.js
@@ -14,20 +14,29 @@ const getLibri = function () {
       const contLibri = document.getElementById("libreria");
       arraybook.forEach((book) => {
         const card = document.createElement("div");
-        card.classList.add("col", "col-md-3", "col-lg-4");
+        card.classList.add("col-12", "col-md-4", "col-xl-3");
         card.innerHTML = `<div class="card" id="card">
             <img src="${book.img}" class="card-img-top" alt="..." />
             <div class="card-body">
               <h5 class="card-title">${book.title}</h5>
               <p class="card-text">
-              <button class="btn btn-primary" id="scarta">scarta</button>
+              <button class="btn btn-danger" id="scarta">scarta</button>
+              <button class="btn btn-primary" id="scarta">aggiungi</button>
                 ${book.price} Euro
               </p>
             </div>`;
         contLibri.appendChild(card);
-        const button = card.querySelector(".btn-primary");
-        button.addEventListener("click", function () {
+        const buttonscarta = card.querySelector(".btn-danger");
+        const buttonaggiungi = card.querySelector(".btn-primary");
+
+        buttonscarta.addEventListener("click", function () {
           card.classList.add("d-none");
+        });
+        buttonaggiungi.addEventListener("click", function () {
+          const carrello = document.getElementById("carrello");
+          const libro = document.createElement("li");
+          libro.innerText = `titolo : ${book.title}`;
+          carrello.appendChild(libro);
         });
       });
     })
